9d0f94e39d40876fa0eb571fbf554f288cae605d
Dhiradj Tangali <90622013+DhiradjT@users.noreply.github.com>
2023-03-29 14:50:16 +0200

    interactive map oefening

---
+++ themaweek/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Interactive Map | Westfield Mall of the Netherlands</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+   
+
+<h1 class="text-center">Interactive Map</h1>
+<div class="buttons text-center">
+    <a href="#">Clothing Stores</a> |
+    <a href="#">Supermarkets</a> |
+    <a href="#">Drug Stores</a> |
+    <a href="#">Self Care Stores</a> |
+    <a href="#">Eletronic Stores</a>
+</div>
+
+<!-- <img class="text-center" src="image_map/balasinski-logistics-intralogistics-robots-agv-autonomouse-vehicles.webp" alt="map"> -->
+
+
+<img src="./imagemap/82b5ccf993bb8a2821fe79654e8eb227.png" alt="imagemap" usemap="#imagemap">
+
+<map name="imagemap">
+  <area shape="polygon" coords="666,208 712,234 713,338 665,312 666,261"  alt="spiegel" href="spiegel.php">
+  <area shape="polygon"  coords="404,355 416,348 481,384 467,395 " alt="trui" href="https://www.deepl.com/nl/translator#nl/en/drogisterij">
+  <area alt="kledingkast" title="kledingkast" href="https://www.youtube.com" coords="739,358 823,317 824,429 742,472 " shape="polygon">
+</map>
+
+
+<!-- <img src="balasinski-logistics-intralogistics-robots-agv-autonomouse-vehicles.webp" usemap="#image_map">
+<map name="image_map">
+  <area alt="C&A" title="C&A" href="https://www.c-and-a.com/nl/nl/shop?utm_source=google&utm_medium=psm&utm_campaign=nl-psm-pmax&utm_term={}&utm_content=psm-pmax&gclid=Cj0KCQjww4-hBhCtARIsAC9gR3atSvC1pfjNSGk76OD-owrlu4nSS_i8OrOr22rU6mC5qC_71zEkEqMaAr5KEALw_wcB" coords="834,1732 1018,1633 1321,1796 1146,1903 1142,1958 821,1800 " shape="polygon">
+  <area alt="hallo" title="hallo" href="https://cdn.dribbble.com/users/1279311/screenshots/20984293/downloads/balasinski-logistics-intralogistics-robots-agv-autonomouse-vehicles.png" coords="2044,2788 2356,2604 2732,2784 2412,2980 2424,3036 2031,2852 " shape="polygon">
+</map> -->
+
+
+
+</div>
+    </div>
+</div>
+    
+
+</body>
+</html>
